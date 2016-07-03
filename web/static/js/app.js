df44308b306abb4ed2da3a01e0c56fd7972cb1f0
Jay Chetty <jaypaulchetty@gmail.com>
2016-07-03 16:23:00 +0100

    add js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,6 +13,9 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+import _ from "lodash"
+
+import something from "./tester"
 // Import local files
 //
 // Local files can be imported directly using relative
