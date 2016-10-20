156b67f1057034b7ef1abf14dbbc786c9137118d
Jay Chetty <jaypaulchetty@gmail.com>
2016-10-20 13:23:44 +0100

    calculator total points of team members

--- web/static/js/components/groups/Group.jsx
+++ web/static/js/components/groups/Group.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import _ from "lodash"
 
-function Group({group}){
+
+function totalPoints(user, weeks){
+  const parScores = weeks.map((week)=>{ return week.week_par })
+  const par = _.sum(parScores)
+
+  const allFixtures = _.flatten( weeks.map((week)=>{ return week.fixtures }) )
+  console.log('allFixtures', allFixtures)
+  console.log('user', user)
+  const upsetPoints = user.predictions.map((prediction)=>{
+    const fixture = allFixtures.find( (fixture)=>{
+      return fixture.id === prediction.fixture_id
+    })
+    console.log('fixture', fixture)
+    return fixture.upset_modifier
+  })
+  console.log('upset points', upsetPoints)
+  const totalPredictionPoints = _.sum(upsetPoints)
+
+  return par + totalPredictionPoints
+
+}
+
+function Group({group, weeks}){
   if(!group){ return null }
   const userViews = group.users.map((user)=>{
     return <li key={user.id}>
-      {user.email}
+      {user.email} : {totalPoints(user, weeks)}
     </li>
   })
   return(
@@ -20,7 +43,8 @@ const mapStateToProps = (state, { params, route } )=>{
     return group.id === Number(params.groupId)
   })
   return {
-    group: group
+    group: group,
+    weeks: state.weeks.items
   }
 }
 
