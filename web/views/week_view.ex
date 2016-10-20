80aafaedaf962b7f430493fd66712e1f4f373ceb
Jay Chetty <jaypaulchetty@gmail.com>
2016-10-20 12:48:43 +0100

    add par scores to the information sent

--- web/views/week_view.ex
+++ web/views/week_view.ex
@@ -15,6 +15,32 @@ defmodule Underdog.WeekView do
       total_home_team_points = LeaguePointsCalculator.points_to_week( home_team_points, week.number )
       total_away_team_points = LeaguePointsCalculator.points_to_week( away_team_points, week.number )
 
+      is_draw = fixture.away_team_score == fixture.home_team_score
+
+      home_team_favorate = total_home_team_points > total_away_team_points
+      away_team_favorate = total_home_team_points < total_away_team_points
+
+      away_team_upset = home_team_favorate && fixture.away_team_score > fixture.home_team_score
+      home_team_upset = away_team_favorate && fixture.away_team_score < fixture.home_team_score
+
+      home_team_ug_points = UnderdogPointsCalculator.points( total_home_team_points, total_away_team_points )
+      away_team_ug_points = UnderdogPointsCalculator.points( total_away_team_points, total_home_team_points )
+
+      # is_draw = fixture.away_team_score == fixture.home_team_score
+
+      is_upset = away_team_upset || home_team_upset
+
+      par_score = case { is_upset, is_draw } do
+        { false, false } -> 3
+        { _, _ } -> 0
+      end
+
+      upset_modifier = case { is_upset, is_draw } do
+        { true, _ } -> abs( home_team_ug_points - away_team_ug_points)
+        { false, true } -> 0
+        { false, false } -> -3
+      end
+
       %{id: fixture.id,
         start_time: fixture.start_time,
         week_id: fixture.week_id,
@@ -22,8 +48,8 @@ defmodule Underdog.WeekView do
         away_team_id: fixture.away_team_id,
         home_team_score: fixture.home_team_score,
         away_team_score: fixture.away_team_score,
-        home_team_ug_points: UnderdogPointsCalculator.points( total_home_team_points, total_away_team_points ),
-        away_team_ug_points: UnderdogPointsCalculator.points( total_away_team_points, total_home_team_points ),
+        home_team_ug_points: home_team_ug_points,
+        away_team_ug_points: away_team_ug_points ,
         away_team: %{id: fixture.away_team.id,
               name: fixture.away_team.name,
               image: fixture.away_team.image,
@@ -33,14 +59,23 @@ defmodule Underdog.WeekView do
             name: fixture.home_team.name,
             image: fixture.home_team.image,
             points: home_team_points
-        }
+        },
+        is_upset: is_upset,
+        par_score: par_score,
+        upset_modifier: upset_modifier
       }
       end)
+
+      week_par = fixtures
+        |> Enum.map(fn fixture -> fixture.par_score end)
+        |> Enum.sum
+
       %{id: week.id,
         start_date: week.start_date,
         number: week.number,
         season_id: week.season_id,
-        fixtures: fixtures
+        fixtures: fixtures,
+        week_par: week_par
       }
     end)
     %{ data: data }
