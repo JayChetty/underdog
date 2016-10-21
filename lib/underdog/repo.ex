f4654837c988b3f2d22dd83a6c52b76a62210c63
Richard Henry <r.henry1989@gmail.com>
2016-10-21 09:47:06 +0100

    adding repo ex

--- lib/underdog/repo.ex
+++ lib/underdog/repo.ex
@@ -1,5 +1,10 @@
 defmodule Underdog.Repo do
-  use Ecto.Repo, otp_app: :underdog
+  use Ecto.Repo, otp_app: :underdog,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "underdog",
+  pool_size: 10
 
   # adapter: Ecto.Adapters.Postgres,
   # username: "postgres",
