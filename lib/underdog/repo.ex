205d55804fe4b7760977ad7a8aa78900040c7186
Richard Henry <r.henry1989@gmail.com>
2016-10-21 09:45:02 +0100

    updating repo ex

--- lib/underdog/repo.ex
+++ lib/underdog/repo.ex
@@ -1,9 +1,9 @@
 defmodule Underdog.Repo do
   use Ecto.Repo, otp_app: :underdog
 
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "underdog",
-  pool_size: 10
+  # adapter: Ecto.Adapters.Postgres,
+  # username: "postgres",
+  # password: "postgres",
+  # database: "underdog",
+  # pool_size: 10
 end
