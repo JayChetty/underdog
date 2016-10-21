79a8802c6091e966d946ae979b805ca3e86941a1
Richard Henry <r.henry1989@gmail.com>
2016-10-21 09:57:18 +0100

    tweaks

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :underdog, Underdog.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :hello_phoenix, HelloPhoenix.Repo,
+config :underdog, Underdog.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
