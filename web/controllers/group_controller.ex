00f2f0e28f9e8bc50a1b0349a8d1430cc9a27ff8
Richard Henry <r.henry1989@gmail.com>
2016-10-24 15:55:29 +0100

    messages limited to 3 and ordered by date

--- web/controllers/group_controller.ex
+++ web/controllers/group_controller.ex
@@ -6,7 +6,8 @@ defmodule Underdog.GroupController do
   def index(conn, _params) do
     user = Guardian.Plug.current_resource(conn)
     user = Repo.preload(user, :groups)
-    groups = Repo.preload( user.groups, [ { :users, :predictions }, :messages ] )
+    groups = Repo.preload( user.groups, [ { :users, :predictions }, messages: from( m in Underdog.Message, order_by: m.updated_at, limit: 3 ) ] )
+
     render(conn, "index.json", groups: groups)
 
     # groups = Repo.all(Group)
