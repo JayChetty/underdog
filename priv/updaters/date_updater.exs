b43b93c0e67ec3fd4bbf50efba598f648d60e987
Jay Chetty <jaypaulchetty@gmail.com>
2016-10-26 10:49:55 +0100

    add ability to update game week from api

--- priv/updaters/date_updater.exs
+++ priv/updaters/date_updater.exs
@@ -13,24 +13,19 @@ json_file = response.body
 fixtures_data = Poison.decode!(json_file)
 fixtures_list = fixtures_data["fixtures"]
 
+
+#Update fixture dates
 Enum.each( fixtures_list, fn( fixture_map )->
-  # Logger.warn "fixture_map #{ inspect fixture_map}"
   home_team_name = Underdog.FixtureJsonParser.api_name_to_name(fixture_map["homeTeamName"])
   home_team_id = Repo.get_by(Underdog.Team, [ name: home_team_name ]).id
   week_number = fixture_map["matchday"]
   week_id = Repo.get_by(Underdog.Week, number: week_number).id
   fixture = Repo.get_by(Underdog.Fixture, [ home_team_id: home_team_id, week_id: week_id ] )
 
-  # new_result = fixture_map["result"]
   {:ok, start_date_time } = Ecto.DateTime.cast( fixture_map["date"] )
-  # Logger.warn "input new_date_string #{ inspect new_date_string }"
-  # Logger.warn "current date #{ inspect fixture.start_time }"
-
   fixture_params = %{
     start_time: start_date_time,
   }
   changeset = Underdog.Fixture.changeset( fixture, fixture_params )
   {:ok, _ } = Repo.update( changeset )
-
-
 end)
