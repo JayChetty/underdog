61399be41513ad4f0d6b449952fa01fc31a15e4b
Richard Henry <r.henry1989@gmail.com>
2016-10-27 15:27:06 +0100

    adding tag UI in for success results

--- web/static/js/components/week/Fixture.jsx
+++ web/static/js/components/week/Fixture.jsx
@@ -3,10 +3,13 @@ import _ from 'lodash'
 
 function Fixture( {fixture, makePrediction, deletePrediction, isGameWeek, isInPast, prediction, matchesInPlay} ){
 
-  let homeTeamClasses = "split-list-view-left"
-  let awayTeamClasses = "split-list-view-right"
-  let homeTeamPointsClasses = "tag go-left"
-  let awayTeamPointsClasses = "tag go-right"
+  let homeTeamClasses = "split-list-view-left layout-flex-grow-6 layout-flex layout-flex-center-vertical layout-justify-flex-space-between"
+  let awayTeamClasses = "split-list-view-right layout-flex-grow-6 layout-flex layout-flex-center-vertical layout-justify-flex-space-between"
+  let homeTeamPointsClasses = "tag"
+  let awayTeamPointsClasses = "tag"
+
+  let tagAnimationHome = null;
+  let tagAnimationAway = null;
 
   let clickHandler = ()=>{ console.log("NOT IN GAME WEEK") };
 
@@ -35,36 +38,51 @@ function Fixture( {fixture, makePrediction, deletePrediction, isGameWeek, isInPa
         const homeTeamWon = fixture.home_team_score > fixture.away_team_score
         const awayTeamWon = fixture.home_team_score < fixture.away_team_score
         if( (homeTeamPredictedWinner && !predictUpset) || (!homeTeamPredictedWinner && predictUpset)  ){
-          homeTeamClasses += " bg-light-blue"
-          homeTeamPointsClasses += " tag-simple pulse"
+          homeTeamClasses += " bg-light-gray"
+          homeTeamPointsClasses = "tag tag-simple bg-warning"
+          tagAnimationHome = <div className="tag tag-animate tag-warning slide-left" ><i className="fa fa-times" aria-hidden="true"></i></div>
           if( homeTeamWon ){
-            homeTeamPointsClasses += " bg-green"
+            homeTeamPointsClasses = "tag tag-simple bg-green pulse"
+            tagAnimationHome = <div className="tag tag-animate tag-success slide-left" ><i className="fa fa-check" aria-hidden="true"></i></div>
           }
 
         }else{
-          awayTeamClasses += " bg-light-blue"
-          awayTeamPointsClasses += " tag-simple pulse"
+          awayTeamClasses += " bg-light-gray"
+          awayTeamPointsClasses = "tag tag-simple bg-warning"
+          tagAnimationAway = <div className="tag tag-animate tag-warning translate-left-20 slide-right" ><i className="fa fa-times" aria-hidden="true"></i></div>
+
           if( awayTeamWon ){
-            awayTeamPointsClasses += " bg-green"
+            awayTeamPointsClasses = "tag tag-simple bg-green pulse"
+            tagAnimationAway = <div className="tag tag-animate tag-success translate-left-20 slide-right" ><i className="fa fa-check" aria-hidden="true"></i></div>
           }
         }
     }
   }
-
+{/*<div className="tag tag-animate tag-success" ><i className="fa fa-check" aria-hidden="true"></i></div>*/}
 
   return(
-    <div className="split-list-view">
+    <div className="split-list-view layout-flex">
       <div className={ homeTeamClasses } onClick={ clickHandler }>
-        <span className={ homeTeamPointsClasses } > { fixture.home_team_ug_points } </span>
-        <span className="text-x-small">{ fixture.home_team.name }</span>
-        <img src={ fixture.home_team.image } />
-        <span> { fixture.home_team_score } </span>
+        <div className="tags">
+          <div className={ homeTeamPointsClasses } > { fixture.home_team_ug_points } </div>
+          { tagAnimationHome }
+        </div>
+        <div>
+          <span className="text-x-small">{ fixture.home_team.name }</span>
+          <img src={ fixture.home_team.image } />
+          <span className="text-small"> { fixture.home_team_score } </span>
+        </div>
       </div>
       <div className={ awayTeamClasses } onClick={ clickHandler }>
-        <span> { fixture.away_team_score } </span>
-        <img src={ fixture.away_team.image } />
-        <span className="text-x-small">{ fixture.away_team.name }</span>
-        <span className={ awayTeamPointsClasses }> {  fixture.away_team_ug_points  } </span>
+        <div>
+          <span className="text-small">{ fixture.away_team_score }</span>
+          <img src={ fixture.away_team.image } />
+          <span className="text-x-small">{ fixture.away_team.name }</span>
+        </div>
+        <div className="tags">
+          { tagAnimationAway }
+          <div className={ awayTeamPointsClasses }>{  fixture.away_team_ug_points  }</div>
+        </div>
       </div>
     </div>
   )
