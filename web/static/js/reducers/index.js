07decff05550b9921ac04e9094a9f3b44b9eb459
Jay Chetty <jaypaulchetty@gmail.com>
2016-10-27 11:18:24 +0100

    remote legacy reducers

--- web/static/js/reducers/index.js
+++ web/static/js/reducers/index.js
@@ -1,16 +1,12 @@
 import { combineReducers } from 'redux'
-import teams from './teams'
-import fixtures from './fixtures'
 import predictions from './predictions'
 import session from './session'
 import weeks from './weeks'
 import groups from './groups'
 
 export default combineReducers({
-  teams,
   predictions,
   session,
-  fixtures,
   weeks,
   groups
 })
