0b27f8d4ea1fb441e6d56b52ee53ca4733a5a171
Richard Henry <r.henry1989@gmail.com>
2016-11-03 15:29:47 +0000

    added in Link to click on in app notify

--- web/static/js/components/InAppNotify.jsx
+++ web/static/js/components/InAppNotify.jsx
@@ -1,18 +1,21 @@
 import React from "react"
+import { Link } from "react-router"
 
 const InAppNotify = ( { notify, inGroups, notificationGroup, isSender } ) => {
   if ( !notify.isNotifying || inGroups || isSender ) { return null; }
 
   return(
-    <div className="toast slide-in-and-out layout-flex">
-      <div className="layout-flex layout-flex-center text-large layout-flex-grow-2">
-        <i className="fa fa-commenting-o" aria-hidden="true"></i>
+    <Link to={`/groups/${ notify.groupId }/chat`}>
+      <div className="toast slide-in-and-out layout-flex">
+        <div className="layout-flex layout-flex-center text-large layout-flex-grow-2">
+          <i className="fa fa-commenting-o" aria-hidden="true"></i>
+        </div>
+        <div className="layout-flex-grow-10">
+          <div className="text-up text-small text-bolder">{ notificationGroup }</div>
+          <div>{ notify.userName }: { notify.body }</div>
+        </div>
       </div>
-      <div className="layout-flex-grow-10">
-        <div className="text-up text-small text-bolder">{ notificationGroup }</div>
-        <div>{ notify.userName }: { notify.body }</div>
-      </div>
-    </div>
+    </Link>
   )
 }
 
